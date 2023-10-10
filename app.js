17c5b694c0e93e9bd0076a9c0a2f84f7c448b12d
Safa Al-Hilo <116551942+Saf-Awi@users.noreply.github.com>
2023-10-10 20:27:52 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+const openMenu = document.getElementById('openMenu');
+const sideBar = document.getElementById('sideNav');
+const closeMenu = document.getElementById('closeMenu');
+
+function openSideBar () {
+    sideBar.style.display = 'flex';
+
+}
+
+function closeSideBar () {
+    sideBar.style.display = 'none';
+}
+
+function returnDefault () {
+    sideBar.style.display = 'flex';
+
+}
+
+
+function handleWindowResize() {
+    const windowWidth = window.innerWidth;
+
+    if (windowWidth > 768) {
+        openSideBar(); 
+        returnDefault();
+        openMenu.style.display = 'none'; 
+        openMenu.removeEventListener('click', openSideBar);
+    } else {
+        openMenu.style.display = 'block';
+        openMenu.addEventListener('click', openSideBar);
+    }
+}
+
+handleWindowResize();
+
+window.addEventListener('resize', handleWindowResize);
+closeMenu.addEventListener('click', closeSideBar);
+
+
+
